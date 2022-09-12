b5871631d48f86097192212faeb68a7db9728ae5
Naveen Agarwal <naveenagarwal287@gmail.com>
2022-09-12 11:46:58 +0530

    fixed the grpc method override

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -89,10 +89,17 @@ export class UserController {
 
   @GrpcMethod('UserController', 'Register')
   async register(data: any): Promise<any> {
-    return await this.userService.register(data.user);
+    console.log('user register')
+    try {
+      const response = await this.userService.register(data.user);
+      return response;
+    } catch (e) {
+      console.log(e);
+      throw e;
+    }
   }
 
-  @GrpcMethod('UserController', 'Register')
+  @GrpcMethod('UserController', 'RegisterMerchant')
   async registerMerchant(data: any): Promise<any> {
     return await this.userService.registerMerchant(data.user);
   }
